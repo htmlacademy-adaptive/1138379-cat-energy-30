ed11d8cf4d61756042b51aeca5ff4ea16e8bb904
alexsoft <alexsoft.js@gmail.com>
2023-12-05 02:30:17 +0700

    Главная страница (правки)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,10 @@
 // menu
-let menu = document.querySelector(".header__button-menu");
-let inner = document.querySelector(".header__menu");
+// let menu = document.querySelector(".header__button-menu");
+// let inner = document.querySelector(".header__menu");
 
-menu.onclick = function () {
-  menu.classList.toggle("header__button-menu--open");
-  menu.classList.toggle("header__button-menu--close");
-  inner.classList.toggle("header__menu--show");
-};
+// menu.onclick = function () {
+//   menu.classList.toggle("header__button-menu--open");
+//   menu.classList.toggle("header__button-menu--close");
+//   inner.classList.toggle("header__menu--show");
+// };
 
