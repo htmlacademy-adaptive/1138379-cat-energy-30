63867d9dae3a6846b3c548ca057b77647fd28643
alexsoft <alexsoft.js@gmail.com>
2023-12-05 01:28:36 +0700

    Главная страница

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+"use strict";
+
+// menu
+let menu = document.querySelector(".header__button-menu");
+let inner = document.querySelector(".header__menu");
+
+menu.onclick = function () {
+  menu.classList.toggle("header__button-menu--open");
+  menu.classList.toggle("header__button-menu--close");
+  inner.classList.toggle("header__menu--show");
+};
+
